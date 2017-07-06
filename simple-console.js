c6be5069307a3a9db85b4e810471ca6f193be9bd
gw0 [http://gw.tnode.com/] <gw.2017@ena.one>
2017-07-06 12:34:53 +0200

    Remove semicolons and lines to pass ESLint config standard check.

--- simple-console.js
+++ simple-console.js
@@ -17,215 +17,218 @@
 
 var SimpleConsole = function (options) {
   if (!options.handleCommand && !options.outputOnly) {
-    throw new Error('You must specify either options.handleCommand(input) or options.outputOnly');
+    throw new Error('You must specify either options.handleCommand(input) or options.outputOnly')
   }
 
-  var outputOnly = options.outputOnly;
-  var handleCommand = options.handleCommand;
-  var placeholder = options.placeholder || '';
-  var autofocus = options.autofocus;
-  var storageId = options.storageID || 'simple-console';
+  var outputOnly = options.outputOnly
+  var handleCommand = options.handleCommand
+  var placeholder = options.placeholder || ''
+  var autofocus = options.autofocus
+  var storageId = options.storageID || 'simple-console'
 
   var addSVG = function (toElement, iconClassName, svg, viewBox = '0 0 16 16') {
-    var icon = document.createElement('span');
-    icon.className = iconClassName;
-    icon.innerHTML = '<svg width="1em" height="1em" viewBox="' + viewBox + '">' + svg + '</svg>';
-    toElement.insertBefore(icon, toElement.firstChild);
-  };
+    var icon = document.createElement('span')
+    icon.className = iconClassName
+    icon.innerHTML = '<svg width="1em" height="1em" viewBox="' + viewBox + '">' + svg + '</svg>'
+    toElement.insertBefore(icon, toElement.firstChild)
+  }
 
   var addChevron = function (toElement) {
     addSVG(toElement, 'input-chevron',
       '<path d="M6,4L10,8L6,12" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>'
-    );
-  };
-
-  var consoleElement = document.createElement('div');
-  consoleElement.className = 'simple-console';
-
-  var output = document.createElement('div');
-  output.className = 'simple-console-output';
-  output.setAttribute('role', 'log');
-  output.setAttribute('aria-live', 'polite');
-
-  var inputWrapper = document.createElement('div');
-  inputWrapper.className = 'simple-console-input-wrapper';
-  addChevron(inputWrapper);
-
-  var input = document.createElement('input');
-  input.className = 'simple-console-input';
-  input.setAttribute('autofocus', 'autofocus');
-  input.setAttribute('placeholder', placeholder);
-  input.setAttribute('aria-label', placeholder);
-
-  consoleElement.appendChild(output);
-  if (!outputOnly) {
-    consoleElement.appendChild(inputWrapper);
+    )
   }
-  inputWrapper.appendChild(input);
+
+  var consoleElement = document.createElement('div')
+  consoleElement.className = 'simple-console'
+
+  var output = document.createElement('div')
+  output.className = 'simple-console-output'
+  output.setAttribute('role', 'log')
+  output.setAttribute('aria-live', 'polite')
+
+  var inputWrapper = document.createElement('div')
+  inputWrapper.className = 'simple-console-input-wrapper'
+  addChevron(inputWrapper)
+
+  var input = document.createElement('input')
+  input.className = 'simple-console-input'
+  input.setAttribute('autofocus', 'autofocus')
+  input.setAttribute('placeholder', placeholder)
+  input.setAttribute('aria-label', placeholder)
+
+  consoleElement.appendChild(output)
+  if (!outputOnly) {
+    consoleElement.appendChild(inputWrapper)
+  }
+  inputWrapper.appendChild(input)
 
   var addButton = function (action) {
-    var button = document.createElement('button');
-    inputWrapper.appendChild(button);
-    button.addEventListener('click', action);
-    return button;
-  };
+    var button = document.createElement('button')
+    inputWrapper.appendChild(button)
+    button.addEventListener('click', action)
+    return button
+  }
 
   var clear = function () {
-    output.innerHTML = '';
-  };
+    output.innerHTML = ''
+  }
 
-  var lastEntry;
+  var lastEntry
   var getLastEntry = function () {
-    return lastEntry;
-  };
+    return lastEntry
+  }
 
   var log = function (content) {
-    var wasScrolledToBottom = output.is_scrolled_to_bottom();
+    var wasScrolledToBottom = output.is_scrolled_to_bottom()
 
-    var entry = document.createElement('div');
-    entry.className = 'entry';
+    var entry = document.createElement('div')
+    entry.className = 'entry'
     if (content instanceof Element) {
-      entry.appendChild(content);
+      entry.appendChild(content)
     } else {
-      entry.innerText = entry.textContent = content;
+      entry.innerText = entry.textContent = content
     }
-    output.appendChild(entry);
+    output.appendChild(entry)
 
     requestAnimationFrame(function () {
       if (wasScrolledToBottom) {
-        output.scroll_to_bottom();
+        output.scroll_to_bottom()
       }
-    });
+    })
 
-    lastEntry = entry;
-    return entry;
-  };
+    lastEntry = entry
+    return entry
+  }
 
   var logHTML = function (html) {
-    log('');
-    getLastEntry().innerHTML = html;
-  };
+    log('')
+    getLastEntry().innerHTML = html
+  }
 
   var error = function (content) {
-    log(content);
-    getLastEntry().classList.add('error');
-  };
+    log(content)
+    getLastEntry().classList.add('error')
+  }
 
   var warn = function (content) {
-    log(content);
-    getLastEntry().classList.add('warning');
-  };
+    log(content)
+    getLastEntry().classList.add('warning')
+  }
 
   var info = function (content) {
-    log(content);
-    getLastEntry().classList.add('info');
-  };
+    log(content)
+    getLastEntry().classList.add('info')
+  }
 
   var success = function (content) {
-    log(content);
-    getLastEntry().classList.add('success');
-  };
+    log(content)
+    getLastEntry().classList.add('success')
+  }
 
   output.is_scrolled_to_bottom = function () {
     // 1px margin of error needed in case the user is zoomed in
-    return output.scrollTop + output.clientHeight + 1 >= output.scrollHeight;
-  };
+    return output.scrollTop + output.clientHeight + 1 >= output.scrollHeight
+  }
 
   output.scroll_to_bottom = function () {
-    output.scrollTop = output.scrollHeight;
-  };
+    output.scrollTop = output.scrollHeight
+  }
 
-  var commandHistory = [];
-  var commandId = commandHistory.length;
-  var commandHistoryKey = storageId + ' command history';
+  var commandHistory = []
+  var commandId = commandHistory.length
+  var commandHistoryKey = storageId + ' command history'
 
   var loadCommandHistory = function () {
     try {
-      commandHistory = JSON.parse(localStorage[commandHistoryKey]);
-      commandId = commandHistory.length;
+      commandHistory = JSON.parse(localStorage[commandHistoryKey])
+      commandId = commandHistory.length
     } catch (e) {}
-  };
+  }
 
   var saveCommandHistory = function () {
     try {
-      localStorage[commandHistoryKey] = JSON.stringify(commandHistory);
+      localStorage[commandHistoryKey] = JSON.stringify(commandHistory)
     } catch (e) {}
-  };
+  }
 
   var clearCommandHistory = function () {
-    commandHistory = [];
-    saveCommandHistory();
-  };
+    commandHistory = []
+    saveCommandHistory()
+  }
 
-  loadCommandHistory();
+  loadCommandHistory()
 
   input.addEventListener('keydown', function (e) {
     if (e.keyCode === 13) { // Enter
-      var command = input.value;
+      // Execute command and add to history
+      var command = input.value
       if (command === '') {
-        return;
+        return
       }
-      input.value = '';
+      input.value = ''
 
       if (commandHistory[commandHistory.length - 1] !== command) {
-        commandHistory.push(command);
+        commandHistory.push(command)
       }
-      commandId = commandHistory.length;
-      saveCommandHistory();
+      commandId = commandHistory.length
+      saveCommandHistory()
 
-      var commandEntry = log(command);
-      commandEntry.classList.add('input');
-      addChevron(commandEntry);
+      var commandEntry = log(command)
+      commandEntry.classList.add('input')
+      addChevron(commandEntry)
 
-      output.scroll_to_bottom();
+      output.scroll_to_bottom()
 
-      handleCommand(command);
-
+      handleCommand(command)
     } else if (e.keyCode === 38) { // Up
+      // Previous command from history
       if (--commandId < 0) {
-        commandId = -1;
-        input.value = '';
+        commandId = -1
+        input.value = ''
       } else {
-        input.value = commandHistory[commandId];
+        input.value = commandHistory[commandId]
       }
-      input.setSelectionRange(input.value.length, input.value.length);
-      e.preventDefault();
-
+      input.setSelectionRange(input.value.length, input.value.length)
+      e.preventDefault()
     } else if (e.keyCode === 40) { // Down
+      // Next command from history
       if (++commandId >= commandHistory.length) {
-        commandId = commandHistory.length;
-        input.value = '';
+        commandId = commandHistory.length
+        input.value = ''
       } else {
-        input.value = commandHistory[commandId];
+        input.value = commandHistory[commandId]
       }
-      input.setSelectionRange(input.value.length, input.value.length);
-      e.preventDefault();
-
+      input.setSelectionRange(input.value.length, input.value.length)
+      e.preventDefault()
     } else if (e.keyCode === 46 && e.shiftKey) { // Shift+Delete
+      // Remove command from history
       if (input.value === commandHistory[commandId]) {
-        commandHistory.splice(commandId, 1);
+        commandHistory.splice(commandId, 1)
         commandId = Math.max(0, commandId - 1)
-        input.value = commandHistory[commandId] || '';
-        saveCommandHistory();
+        input.value = commandHistory[commandId] || ''
+        saveCommandHistory()
       }
-      e.preventDefault();
+      e.preventDefault()
     }
-  });
+  })
 
-  this.element = consoleElement;
-  this.input = input;
-  this.addButton = addButton;
+  this.element = consoleElement
+  this.input = input
+  this.addButton = addButton
 
   this.handleUncaughtErrors = function () {
-    window.onerror = error;
-  };
+    window.onerror = error
+  }
 
-  this.log = log;
-  this.logHTML = logHTML;
-  this.error = error;
-  this.warn = warn;
-  this.info = info;
-  this.success = success;
-  this.getLastEntry = getLastEntry;
-  this.clear = clear;
-};
+  this.log = log
+  this.logHTML = logHTML
+  this.error = error
+  this.warn = warn
+  this.info = info
+  this.success = success
+  this.getLastEntry = getLastEntry
+  this.clear = clear
+}
+
+module.exports = SimpleConsole
